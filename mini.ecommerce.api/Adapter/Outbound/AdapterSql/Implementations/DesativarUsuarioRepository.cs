c3ff0edb61111ff79b47df6a0dfc93954e90e0eb
Ramon Souza <momon14souza@gmail.com>
2026-06-07 13:12:53 -0300

    feat: adição do repository de desativação do usuario

---
+++ mini.ecommerce.api/Adapter/Outbound/AdapterSql/Implementations/DesativarUsuarioRepository.cs
@@ -0,0 +1,62 @@
+using System.Data;
+using System.Diagnostics;
+using System.Text.Json;
+using Dapper;
+using mini.ecommerce.api.Adapter.Outbound.AdapterSql.Interfaces;
+using mini.ecommerce.api.Adapter.Outbound.AdapterSql.Settings;
+using mini.ecommerce.api.Domain.Core.Model.VM.Response;
+
+namespace mini.ecommerce.api.Adapter.Outbound.AdapterSql.Implementations;
+
+public class DesativarUsuarioRepository(IPostgreSQLConnection connection) : IDesativarUsuarioRepository
+{
+    private readonly IPostgreSQLConnection _connection = connection;
+    
+    public async ValueTask<DesativarUsuarioFunctionResponse> LoginUsuario(int usuarioId)
+    {
+        using var activity = Activity.Current?.Source.StartActivity("desativar-usuario-repository");
+        using var connection = _connection.ConnectCLUST("PROSEL_LAPES");
+        
+        string functionName = "fn_usuario_desativar";
+        
+        activity?.SetTag("procedure_name", functionName);
+
+        var parameters = new DynamicParameters();
+        
+        parameters.Add("p_usuario_id", usuarioId, DbType.Int32, ParameterDirection.Input);
+        
+        activity?.SetTag("mensagem_in", JsonSerializer.Serialize(new { usuarioId }));
+        
+        var sql =
+            $"SELECT {functionName}(" +
+            "@p_usuario_id" +
+            ");";
+
+        var jsonResponse = await connection.QueryFirstOrDefaultAsync<string>(sql, parameters);
+
+        if (string.IsNullOrWhiteSpace(jsonResponse))
+        {
+            throw new Exception(
+                "Nenhuma resposta retornada pela função.");
+        }
+
+        activity?.SetTag("mensagem_out", jsonResponse);
+
+        var response =
+            JsonSerializer.Deserialize<
+                DesativarUsuarioFunctionResponse>(
+                jsonResponse,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+        if (response is null)
+        {
+            throw new Exception(
+                "Erro ao desserializar resposta.");
+        }
+
+        return response;
+    }
+}
---
+++ mini.ecommerce.api/Adapter/Outbound/AdapterSql/Interfaces/IDesativarUsuarioRepository.cs
@@ -0,0 +1,8 @@
+using mini.ecommerce.api.Domain.Core.Model.VM.Response;
+
+namespace mini.ecommerce.api.Adapter.Outbound.AdapterSql.Interfaces;
+
+public interface IDesativarUsuarioRepository
+{
+    public ValueTask<DesativarUsuarioFunctionResponse> LoginUsuario(int usuarioId);
+}
